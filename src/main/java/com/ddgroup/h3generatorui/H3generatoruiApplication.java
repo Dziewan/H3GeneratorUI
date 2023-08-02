f1bd1b6183107418580489378b1a63fdd65668f2
mdziewanowski <m.dziewanowski@ambsoft.pl>
2023-08-02 15:50:51 +0200

    init

---
+++ src/main/java/com/ddgroup/h3generatorui/H3generatoruiApplication.java
@@ -0,0 +1,13 @@
+package com.ddgroup.h3generatorui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H3generatoruiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H3generatoruiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddgroup/h3generatorui/H3generatoruiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddgroup.h3generatorui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H3generatoruiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
